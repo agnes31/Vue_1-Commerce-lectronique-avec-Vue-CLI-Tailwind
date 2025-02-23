a9c345f87da1f2672c027e3501cbe26fb7251c27
agnes31 <agnesomet@gmail.com>
2025-02-23 13:07:46 +0700

    Configure Tailwind CSS in a Vue.js project

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+// filepath: /Users/agnesdev/Ecole/my-vue-project/postcss.config.js
+module.exports = {
+    plugins: {
+      // '@tailwindcss/postcss': {},
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png" class="mx-auto">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="text-3xl font-bold underline text-yellow-500 mt-6">
+      Hello world!
+    </h1>
   </div>
 </template>
 
